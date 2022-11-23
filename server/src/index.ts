d2ed75961cdc171a538bf5629a0b22bf829cb91c
Theo Dullin <theo@kili-technology.com>
2022-11-23 20:44:45 +0100

    fix build index.html

--- server/src/index.ts
+++ server/src/index.ts
@@ -8,9 +8,9 @@ import io from './socket';
 const app = express();
 app.use(cors());
 if (process.env.NODE_ENV !== 'development') {
-  app.use(express.static(path.resolve(__dirname, '../client/build')));
+  app.use(express.static(path.resolve(__dirname, '../../client/build')));
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+    res.sendFile(path.join(__dirname, '../../client/build/index.html'));
   });
 }
 
