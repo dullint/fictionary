25b33214e7ebdcf38fc31dc021c3e93cf18901b7
Theo Dullin <theo@kili-technology.com>
2023-02-15 10:08:00 +0100

    update socket types

--- server/src/socket/types.ts
+++ server/src/socket/types.ts
@@ -13,41 +13,56 @@ export interface SocketData {
 export type RoomIdPayload = { roomId: RoomId };
 
 export interface ServerToClientEvents {
+  //join room
   join_room_error: (error: JoinRoomError) => void;
   room_joined: (room: ClientRoom) => void;
+
+  //get room
+  room_existence: (existence: boolean) => void;
   room_error: (error: RoomError) => void;
+
+  //create room
   room_created: () => void;
+  create_room_error: (error: CreateRoomError) => void;
+
+  //update username
   username_updated: () => void;
   update_username_error: (error: UpdateUsernameError) => void;
-  create_room_error: (error: CreateRoomError) => void;
+
+  //update timer
   timer: (counter: number) => void;
-  room_existence: (existence: boolean) => void;
+
+  //update room to client
   room: (room: ClientRoom) => void;
 }
 
 export interface ClientToServerEvents {
-  update_username: (payload: UpdateUsernamePayload) => void;
+  //in Home
   check_room_existence: (payload: RoomIdPayload) => void;
   create_room: (payload: RoomIdPayload) => void;
   join_room: (payload: RoomIdPayload) => void;
+
+  //in waiting room
   leave_room: (payload: RoomIdPayload) => void;
-  onDisconnect: (payload: RoomIdPayload) => void;
-  players: () => void;
+  update_username: (payload: UpdateUsernamePayload) => void;
   room_joined: () => void;
-  room_created: () => void;
+  change_game_settings: (payload: ChangeGameSettingsPayload) => void;
   launch_game: (payload: RoomIdPayload) => void;
-  username_updated: () => void;
-  update_username_error: () => void;
-  update_scores: ({ roomId, scores }: UpdateScoresPayload) => void;
-  newRound: (payload: RoomIdPayload) => void;
-  show_results: () => void;
+
+  // *** In game
+  //in Prompt
+  get_new_word: () => void;
   submit_definition: (payload: SubmitDefinitionPayload) => void;
   remove_definition: (payload: RemoveDefinition) => void;
+  show_results: () => void;
+
+  //in guess
+  update_scores: (payload: UpdateScoresPayload) => void;
+
+  //in result
   new_round: (payload: RoomIdPayload) => void;
   reset_game: (payload: RoomIdPayload) => void;
   select_definition: (payload: SelectDefinitionPayload) => void;
-  get_new_word: () => void;
-  change_game_settings: (payload: ChangeGameSettingsPayload) => void;
 }
 
 export interface UpdateScoresPayload {
