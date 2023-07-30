f5a9aa547addaeb4fa7920386bcd6b5c548e3a80
dullint <theo.dullin@orange.fr>
2023-07-30 14:16:40 +0200

    fix credits

--- client/src/components/CreditsDialog/index.tsx
+++ client/src/components/CreditsDialog/index.tsx
@@ -27,33 +27,40 @@ const CreditsDialog = (props: PropsType) => {
           Credits
         </Typography>
         <Typography variant="subtitle1">Avatars:</Typography>
-        <Box display={'flex'}>
+        <Typography>
           <Link
             href="https://www.figma.com/community/file/966199982810283152"
             sx={{ marginRight: 0.5 }}
             target="_blank"
             alignSelf="center"
+            component={'span'}
           >
             Croodles - Doodle your face
           </Link>
-          <Typography sx={{ marginRight: 0.5 }}>by</Typography>
+          <Typography sx={{ marginRight: 0.5 }} component={'span'}>
+            by
+          </Typography>
           <Link
             href="https://vijayverma.co/"
             sx={{ marginRight: 0.5 }}
             target="_blank"
             alignSelf="center"
+            component={'span'}
           >
             vijay verma
           </Link>
-          <Typography sx={{ marginRight: 0.5 }}>licensed under</Typography>
+          <Typography sx={{ marginRight: 0.5 }} component={'span'}>
+            licensed under
+          </Typography>
           <Link
             href="https://creativecommons.org/licenses/by/4.0/"
             target="_blank"
             alignSelf="center"
+            component={'span'}
           >
             CC BY 4.0
           </Link>
-        </Box>
+        </Typography>
       </DialogContent>
     </Dialog>
   );
