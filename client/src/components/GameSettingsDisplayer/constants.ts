fb4224fcb74e34fb7d0ae924d0cabc5eacc79294
dullint <theo.dullin@orange.fr>
2023-07-30 14:27:08 +0200

    change game settings constants

--- client/src/components/GameSettingsDisplayer/constants.ts
+++ client/src/components/GameSettingsDisplayer/constants.ts
@@ -1,4 +1,4 @@
 export const promptTimeOptions = [0.5, 1, 1.5, 3, 4];
-export const roundNumberOptions = [2, 3, 4, 5, 6];
+export const roundNumberOptions = [3, 4, 5, 7, 10];
 export const useExampleOptions = [true, false];
 export const showGuessVoteOptions = [true, false];
