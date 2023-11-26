5a376fb73b0a931e34222b20908824ef22a19488
dullint <theo.dullin@orange.fr>
2023-11-26 18:56:12 +0100

    log ip adress

--- server/src/socket/index.ts
+++ server/src/socket/index.ts
@@ -1,15 +1,15 @@
-import { Server } from 'socket.io';
-import { Server as HTTPServer } from 'http';
-import { PING_INTERVAL, PING_TIMEOUT } from './constants';
-import mixpanel from '../mixpanel';
+import { Server } from "socket.io";
+import { Server as HTTPServer } from "http";
+import { PING_INTERVAL, PING_TIMEOUT } from "./constants";
+import mixpanel from "../mixpanel";
 import {
   ClientToServerEvents,
   ServerToClientEvents,
   SocketData,
-} from './types';
-import { roomHandler } from '../handler/roomHandler';
-import logger from '../logging';
-import { gameHandler } from '../handler/gameHandler';
+} from "./types";
+import { roomHandler } from "../handler/roomHandler";
+import logger from "../logging";
+import { gameHandler } from "../handler/gameHandler";
 
 export default (server: HTTPServer) => {
   const io = new Server<ClientToServerEvents, ServerToClientEvents, SocketData>(
@@ -17,11 +17,11 @@ export default (server: HTTPServer) => {
     {
       cors: {
         origin: [
-          'http://localhost:3021',
-          'http://192.168.1.66:3021',
-          'http://172.20.10.3:3021',
+          "http://localhost:3021",
+          "http://192.168.1.66:3021",
+          "http://172.20.10.3:3021",
         ],
-        methods: ['GET', 'POST'],
+        methods: ["GET", "POST"],
       },
       pingInterval: PING_INTERVAL,
       pingTimeout: PING_TIMEOUT,
@@ -31,12 +31,13 @@ export default (server: HTTPServer) => {
   io.use((socket, next) => {
     const userId = socket.handshake.auth.userId;
     const ip = socket.request.socket.remoteAddress;
+    console.log(ip);
     socket.data.userId = userId;
     socket.data.ip = ip;
     return next();
   });
 
-  io.on('connection', (socket) => {
+  io.on("connection", (socket) => {
     logger.info(
       `[USER ${socket.data.userId}] User connected with socket Id ${socket.id}`
     );
@@ -45,7 +46,7 @@ export default (server: HTTPServer) => {
     roomHandler(io, socket);
     gameHandler(io, socket);
 
-    socket.on('disconnect', async () => {
+    socket.on("disconnect", async () => {
       logger.info(`[USER ${socket.data.userId}] User disconnected`);
     });
   });
